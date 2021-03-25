2454ca6fff1ab54410a27b8ac73b237f48378f85
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2021-03-25 12:29:58 +0900

    0325 class

---
+++ comAlgo5_1/comAlgo5_1/main.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<Windows.h>
+
+#define MAX_SIZE 15
+#define SWAP(x,y,t) ((t)=(x),(x)=(y),(y)=(t))
+
+void selection_sort(int list[], int n) {
+	int least, temp;
+	for (int i = 0; i < n - 1; i++) {
+		least = i;
+		for (int j = i + 1; j < n; j++) {
+			if (list[j] < list[least])
+				least = j;
+		}
+		SWAP(list[i], list[least],temp);
+	}
+}
+
+void insertion_sort(int list[], int n) {
+	int i, j, save;
+	for (i = 1; i < n; i++) {
+		save = list[i];
+		for (j = i - 1; j >= 0 && list[j] > save; j--)
+			list[j + 1] = list[j];
+		list[j + 1] = save;
+	}
+}
+void main() {
+	int list[MAX_SIZE];
+	srand(time(NULL));
+	
+	//selection
+	for (int i = 0; i < MAX_SIZE; i++) {
+		list[i] = rand() % 100;
+		for (int j = 0; j < i; j++)
+			if (list[i] == list[j])
+				i--;
+	}
+
+	for (int i = 0; i < MAX_SIZE; i++)
+		printf("%d ", list[i]);
+	printf("\n\n"); getch();
+
+	selection_sort(list, MAX_SIZE);
+	for (int i = 0; i < MAX_SIZE; i++) {
+		printf("%d ", list[i]);
+		Sleep(500);
+	}
+	printf("\n\n"); getch();
+	for (int i = 0; i < MAX_SIZE; i++) {
+		list[i] = rand() % 100;
+		for (int j = 0; j < i; j++)
+			if (list[i] == list[j])
+				i--;
+	}
+	
+	//insertion
+	for (int i = 0; i < MAX_SIZE; i++)
+		printf("%d ", list[i]);
+	printf("\n\n"); getch();
+	insertion_sort(list, MAX_SIZE);
+	for (int i = 0; i < MAX_SIZE; i++) {
+		printf("%d ", list[i]);
+		Sleep(500);
+	}
+	printf("\n\n"); getch();
+
+
+}
