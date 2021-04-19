04e1c4b774ba0669515a88f01138d3bfa71035b7
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2021-04-19 19:22:14 +0900

    0419 update

---
+++ comAlgo3_2/comAlgo3_2/main.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define N 4
+#define M 5
+void spiral(int A[N][M]) {
+	int left = 0, right = M - 1;
+	int top = 0, bottom = N - 1;
+
+	int value = 1;
+	while (left <= right && top <= bottom) {
+		if (left <= right) {
+			for (int j = left; j <= right; j++) {
+				A[top][j] = value;
+				value++;
+			}
+		}
+		top = top + 1;
+		if (top <= bottom) {
+			for (int i = top; i <= bottom; i++) {
+				A[i][right] = value;
+				value++;
+			}
+
+		}
+		right = right - 1;
+		
+		if (left <= right) {
+			for (int j = right; j >= left; j--) {
+				A[bottom][j] = value;
+				value++;
+			}
+		}
+		bottom -= 1;
+		
+		if (top <= bottom) {
+			for (int i = bottom; i >= top; i--) {
+				A[i][left] = value;
+				value++;
+			}
+
+		}
+		left += 1;
+		//break;
+	}
+}
+
+void printArray(int A[N][M]) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			printf("%2d ", A[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void main() {
+	int A[N][M] = { 0 };
+	spiral(A);
+	printArray(A);
+}
